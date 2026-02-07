9089747ab687972e1fb96383e03901e6f841cb53
helene <helene.maja@hotmail.dk>
2026-02-07 23:46:38 +0100

    set basic example

--- main.c
+++ main.c
@@ -44,24 +44,34 @@
  */
 enum
 {
-  BLINK_NOT_MOUNTED = 250,
-  BLINK_MOUNTED = 1000,
-  BLINK_SUSPENDED = 2500,
+    BLINK_NOT_MOUNTED = 250,
+    BLINK_MOUNTED = 1000,
+    BLINK_SUSPENDED = 2500,
 };
 
 enum col
 {
-  COLA = 5,
-  COLB = 7,
-  COLC = 8,
+    COLA = 5,
+    COLB = 7,
+    COLC = 8,
 };
 
+enum row
+{
+    ROW1 = 4,
+    ROW2 = 6,
+    ROW3 = 9,
+};
 
 static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
 static enum col col_arr[] = {COLA, COLB, COLC};
-volatile int current_col_idx = 0;
-uint8_t key_0 = 1;
-uint8_t key_pressed = 0;
+
+int current_col_idx = 0;
+
+static volatile bool btnA_pressed = false; // (row0,col0) = (GPIO4, GPIO5)
+static volatile bool btnB_pressed = false; // (row0,col1) = (GPIO4, GPIO7)
+
+
 void led_blinking_task(void);
 void hid_task(void);
 void scan_btn_matrix(void);
@@ -70,46 +80,46 @@ void scan_btn_matrix(void);
 int main(void)
 {
 
-  board_init();
+    board_init();
 
-  // init device stack on configured roothub port
-  tud_init(BOARD_TUD_RHPORT);
+    // init device stack on configured roothub port
+    tud_init(BOARD_TUD_RHPORT);
 
-  if (board_init_after_tusb)
-  {
-    board_init_after_tusb();
-  }
+    if (board_init_after_tusb)
+    {
+        board_init_after_tusb();
+    }
 
-  // rows
-  gpio_init(4);
-  gpio_pull_up(4);
-  gpio_set_dir(4, false);
-  gpio_init(6);
-  gpio_pull_up(6);
-  gpio_set_dir(6, false);
-  gpio_init(9);
-  gpio_pull_up(9);
-  gpio_set_dir(9, false);
+    // rows
+    gpio_init(4);
+    gpio_pull_up(4);
+    gpio_set_dir(4, false);
+    gpio_init(6);
+    gpio_pull_up(6);
+    gpio_set_dir(6, false);
+    gpio_init(9);
+    gpio_pull_up(9);
+    gpio_set_dir(9, false);
 
-  // cols
-  gpio_init(5);
-  gpio_set_dir(5, true);
-  gpio_put(5, true);
-  gpio_init(7);
-  gpio_set_dir(7, true);
-  gpio_put(7, true);
-  gpio_init(8);
-  gpio_set_dir(8, true);
-  gpio_put(8, true);
+    // cols
+    gpio_init(5);
+    gpio_set_dir(5, true);
+    gpio_put(5, true);
+    gpio_init(7);
+    gpio_set_dir(7, true);
+    gpio_put(7, true);
+    gpio_init(8);
+    gpio_set_dir(8, true);
+    gpio_put(8, true);
 
-  while (1)
-  {
-    tud_task(); // tinyusb device task
-    led_blinking_task();
+    while (1)
+    {
+        tud_task(); // tinyusb device task
+        led_blinking_task();
 
-    scan_btn_matrix();
-    hid_task();
-  }
+        scan_btn_matrix();
+        hid_task();
+    }
 }
 
 //--------------------------------------------------------------------+
@@ -119,13 +129,13 @@ int main(void)
 // Invoked when device is mounted
 void tud_mount_cb(void)
 {
-  blink_interval_ms = BLINK_MOUNTED;
+    blink_interval_ms = BLINK_MOUNTED;
 }
 
 // Invoked when device is unmounted
 void tud_umount_cb(void)
 {
-  blink_interval_ms = BLINK_NOT_MOUNTED;
+    blink_interval_ms = BLINK_NOT_MOUNTED;
 }
 
 // Invoked when usb bus is suspended
@@ -133,14 +143,14 @@ void tud_umount_cb(void)
 // Within 7ms, device must draw an average of current less than 2.5 mA from bus
 void tud_suspend_cb(bool remote_wakeup_en)
 {
-  (void)remote_wakeup_en;
-  blink_interval_ms = BLINK_SUSPENDED;
+    (void)remote_wakeup_en;
+    blink_interval_ms = BLINK_SUSPENDED;
 }
 
 // Invoked when usb bus is resumed
 void tud_resume_cb(void)
 {
-  blink_interval_ms = tud_mounted() ? BLINK_MOUNTED : BLINK_NOT_MOUNTED;
+    blink_interval_ms = tud_mounted() ? BLINK_MOUNTED : BLINK_NOT_MOUNTED;
 }
 
 //--------------------------------------------------------------------+
@@ -149,61 +159,74 @@ void tud_resume_cb(void)
 
 static void send_hid_report(uint8_t report_id, uint32_t btn)
 {
-  // skip if hid is not ready yet
-  if (!tud_hid_ready())
-    return;
+    // skip if hid is not ready yet
+    if (!tud_hid_ready())
+        return;
 
-  // use to avoid send multiple consecutive zero report for keyboard
-  static bool has_keyboard_key = false;
+    // use to avoid send multiple consecutive zero report for keyboard
+    static bool has_keyboard_key = false;
 
-  if (btn)
-  {
-    uint8_t keycode[6] = {0};
-    keycode[0] = HID_KEY_A;
-    keycode[1] = HID_KEY_B;
+    if (btn)
+    {
+        uint8_t keycode[6] = {0};
+        int k = 0;
 
-    tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
-    has_keyboard_key = true;
-  }
-  else
-  {
-    // send empty key report if previously has key pressed
-    if (has_keyboard_key)
-      tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
-    has_keyboard_key = false;
-  }
+        if (btnA_pressed) keycode[k++] = HID_KEY_A;
+        if (btnB_pressed) keycode[k++] = HID_KEY_B;
 
+        
+
+        tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
+        has_keyboard_key = true;
+    }
+    else
+    {
+        // send empty key report if previously has key pressed
+        if (has_keyboard_key)
+            tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
+        has_keyboard_key = false;
+    }
 }
 
 // Every 10ms, we will sent 1 report for each HID profile (keyboard, mouse etc ..)
 // tud_hid_report_complete_cb() is used to send the next report after previous one is complete
 void hid_task(void)
 {
-  // Poll every 10ms
-  const uint32_t interval_ms = 10;
-  static uint32_t start_ms = 0;
+    // Poll every 10ms
+    const uint32_t interval_ms = 10;
+    static uint32_t start_ms = 0;
 
-  if (board_millis() - start_ms < interval_ms)
-    return; // not enough time
-  start_ms += interval_ms;
+    if (board_millis() - start_ms < interval_ms)
+        return; // not enough time
+    start_ms += interval_ms;
 
-  // uint32_t const btn = board_button_read();
-  // uint32_t const btn = (key_pressed == key_0 && !gpio_get(5));
-  uint32_t const btn = key_pressed;
-  // uint32_t const btn = !gpio_get(4);
+    // uint32_t const btn = board_button_read();
+    // uint32_t const btn = (key_pressed == key_0 && !gpio_get(5));
+    // key_pressed = false;
+    // for (int i =0; i<2; i++)
+    // {
+    //     if (row_idx[i])
+    //     {
+    //         key_pressed = true;
+    //         break;
+    //     }
+    // }
+    bool any = btnA_pressed || btnB_pressed;
+    uint32_t const btn = any;
+    // uint32_t const btn = !gpio_get(4);
 
-  // Remote wakeup
-  if (tud_suspended() && btn)
-  {
-    // Wake up host if we are in suspend mode
-    // and REMOTE_WAKEUP feature is enabled by host
-    tud_remote_wakeup();
-  }
-  else
-  {
-    // Send the 1st of report chain, the rest will be sent by tud_hid_report_complete_cb()
-    send_hid_report(REPORT_ID_KEYBOARD, btn);
-  }
+    // Remote wakeup
+    if (tud_suspended() && btn)
+    {
+        // Wake up host if we are in suspend mode
+        // and REMOTE_WAKEUP feature is enabled by host
+        tud_remote_wakeup();
+    }
+    else
+    {
+        // Send the 1st of report chain, the rest will be sent by tud_hid_report_complete_cb()
+        send_hid_report(REPORT_ID_KEYBOARD, btn);
+    }
 }
 
 // Invoked when sent REPORT successfully to host
@@ -211,9 +234,9 @@ void hid_task(void)
 // Note: For composite reports, report[0] is report ID
 void tud_hid_report_complete_cb(uint8_t instance, uint8_t const *report, uint16_t len)
 {
-  (void)instance;
-  (void)report;
-  (void)len;
+    (void)instance;
+    (void)report;
+    (void)len;
 }
 
 // Invoked when received GET_REPORT control request
@@ -221,47 +244,47 @@ void tud_hid_report_complete_cb(uint8_t instance, uint8_t const *report, uint16_
 // Return zero will cause the stack to STALL request
 uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen)
 {
-  // TODO not Implemented
-  (void)instance;
-  (void)report_id;
-  (void)report_type;
-  (void)buffer;
-  (void)reqlen;
+    // TODO not Implemented
+    (void)instance;
+    (void)report_id;
+    (void)report_type;
+    (void)buffer;
+    (void)reqlen;
 
-  return 0;
+    return 0;
 }
 
 // Invoked when received SET_REPORT control request or
 // received data on OUT endpoint ( Report ID = 0, Type = 0 )
 void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize)
 {
-  (void)instance;
+    (void)instance;
 
-  if (report_type == HID_REPORT_TYPE_OUTPUT)
-  {
-    // Set keyboard LED e.g Capslock, Numlock etc...
-    if (report_id == REPORT_ID_KEYBOARD)
+    if (report_type == HID_REPORT_TYPE_OUTPUT)
     {
-      // bufsize should be (at least) 1
-      if (bufsize < 1)
-        return;
+        // Set keyboard LED e.g Capslock, Numlock etc...
+        if (report_id == REPORT_ID_KEYBOARD)
+        {
+            // bufsize should be (at least) 1
+            if (bufsize < 1)
+                return;
 
-      uint8_t const kbd_leds = buffer[0];
+            uint8_t const kbd_leds = buffer[0];
 
-      if (kbd_leds & KEYBOARD_LED_CAPSLOCK)
-      {
-        // Capslock On: disable blink, turn led on
-        blink_interval_ms = 0;
-        board_led_write(true);
-      }
-      else
-      {
-        // Caplocks Off: back to normal blink
-        board_led_write(false);
-        blink_interval_ms = BLINK_MOUNTED;
-      }
+            if (kbd_leds & KEYBOARD_LED_CAPSLOCK)
+            {
+                // Capslock On: disable blink, turn led on
+                blink_interval_ms = 0;
+                board_led_write(true);
+            }
+            else
+            {
+                // Caplocks Off: back to normal blink
+                board_led_write(false);
+                blink_interval_ms = BLINK_MOUNTED;
+            }
+        }
     }
-  }
 }
 
 //--------------------------------------------------------------------+
@@ -269,62 +292,58 @@ void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_
 //--------------------------------------------------------------------+
 void led_blinking_task(void)
 {
-  static uint32_t start_ms = 0;
-  static bool led_state = false;
+    static uint32_t start_ms = 0;
+    static bool led_state = false;
 
-  // blink is disabled
-  if (!blink_interval_ms)
-    return;
+    // blink is disabled
+    if (!blink_interval_ms)
+        return;
 
-  // Blink every interval ms
-  if (board_millis() - start_ms < blink_interval_ms)
-    return; // not enough time
-  start_ms += blink_interval_ms;
+    // Blink every interval ms
+    if (board_millis() - start_ms < blink_interval_ms)
+        return; // not enough time
+    start_ms += blink_interval_ms;
 
-  board_led_write(led_state);
-  led_state = 1 - led_state; // toggle
+    board_led_write(led_state);
+    led_state = 1 - led_state; // toggle
 }
-
 
 void scan_btn_matrix()
 {
-  // Poll every 1ms
-  const uint32_t interval_ms = 1;
-  static uint32_t start_ms = 0;
-  if (board_millis() - start_ms < interval_ms)
-    return; // not enough time
-  start_ms += interval_ms;
+    // Poll every 1ms
+    const uint32_t interval_ms = 1;
+    static uint32_t start_ms = 0;
+    if (board_millis() - start_ms < interval_ms)
+        return; // not enough time
+    start_ms += interval_ms;
 
+    int active_col = current_col_idx;
 
-  // set all columns high
-  gpio_put(5, true);
-  gpio_put(7, true);
-  gpio_put(8, true);
+    // set all columns high (deactivate)
+    gpio_put(COLA, true);
+    gpio_put(COLB, true);
+    gpio_put(COLC, true);
 
-  // set current col to active
-  gpio_put(col_arr[current_col_idx], false);
-  bool row4 = !gpio_get(4);
-  if (current_col_idx == 1)
-  {
-      key_pressed = !gpio_get(4);   // row is low => pressed
-  }
+    // set current col to active
+    gpio_put(col_arr[active_col], false);
+    sleep_us(2);
+    // read rows
+    bool r0 = !gpio_get(ROW1); //
+    bool r2 = !gpio_get(ROW3); // 
 
-  // wrap to 0 if 2
-  if (++current_col_idx > 2) current_col_idx = 0;
-  // read rows
-  
-  // set next col to active
-  // gpio_put(col_arr[current_col_idx], false);
+    // Update only the key that belongs to this scanned column
+    if (active_col == 0)               
+    {
+        btnA_pressed = r0;
+    }
+    else if (active_col == 2)          
+    {
+        btnB_pressed = r2;
+    }
 
-  // // read all rows, find the row that is pressed
-  // uint8_t row_pressed = ((!gpio_get(4) | !gpio_get(6) | !gpio_get(9))); // 100
+    // advance column
+    current_col_idx++;
+    if (current_col_idx > 2) current_col_idx = 0;
 
-  // uint8_t row_mask = (!gpio_get(4) ? 1u << 0 : 0)
-  //            | (!gpio_get(6) ? 1u << 1 : 0)
-  //            | (!gpio_get(9) ? 1u << 2 : 0);
-  
-
-
-  // key_pressed = row_mask;
 
 }
